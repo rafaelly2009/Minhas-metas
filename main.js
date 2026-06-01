d18198a87a2c5c3c3250f4adfeb0d9d2464fe754
rafaelly2009 <silva.andrade.rafaelly@escola.pr.gov.br>
2026-06-01 08:14:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
+    
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
       textos[j].classList.remove("ativo");
@@ -15,6 +16,22 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-10-05"); 
+const tempoObjetivo1 = new Date("2026-10-05T00:00:00"); 
 
-contadores[0].textContent = "Contagem regressiva";
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60)
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+
+  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
